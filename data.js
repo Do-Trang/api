0a2b55dd04945250513fc619d48427054a501c7a
Trang <thutrang25062k4@gmail.com>
2024-05-26 11:25:46 +0700

    clean

--- data.js
+++ data.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const Student = require('./src/models/student');
 
-// Kết nối đến MongoDB
 const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/student';
 
 mongoose.connect(mongoURI, {
