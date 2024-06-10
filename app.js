2e616ab3710f3e9555d79a484c999dc89f4950d1
Trang <thutrang25062k4@gmail.com>
2024-06-10 22:16:11 +0700

    fix link

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'http://localhost:8090',
+  origin: 'http://192.168.56.10:32135',
   optionsSuccessStatus: 200
 };
 
