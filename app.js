1d828b6c537721f319525ea683ec4633058b045a
Trang <thutrang25062k4@gmail.com>
2024-06-13 01:18:45 +0700

    add helm prom

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const studentRoutes = require('./src/routes/studentRoutes');
 const apiLimiter = require('./rateLimit');
+const promClient = require('prom-client');
 
 const app = express();
 app.use(express.json());
@@ -28,6 +28,30 @@ mongoose.connect(mongoURI, {
   console.log('Connected to MongoDB');
 }).catch(err => {
   console.error('Error connecting to MongoDB', err);
+});
+
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
+
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000] 
+});
+
+
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.route ? req.route.path : req.path, status_code: res.statusCode });
+  });
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
 });
 
 app.use('/api/students', apiLimiter, studentRoutes);
